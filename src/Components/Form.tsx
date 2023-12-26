56f447444ac9c6159fb7cfe84c763ab13032b2c7
Arkhypchuk Andrii <andrewarhypchuk@gmail.com>
2023-12-26 12:07:00 +0200

    optimizing

---
+++ src/Components/Form.tsx
@@ -0,0 +1,9 @@
+import styles from "../Styling/footer.module.css";
+function Form() {
+  return <section className={styles.formPopUpContainer}>
+              <div className={styles.formContainer}>
+
+              </div>
+  </section>;
+}
+export default Form;
--- src/Components/HomePageAboutUs.tsx
+++ src/Components/HomePageAboutUs.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "../Styling/homePage.module.css";
 
 function AboutUsSection() {
@@ -8,22 +9,6 @@ function AboutUsSection() {
         <div className={styles.aboutUsContainer__subTitleText}>
           СТВОРЮЄМО ЗВ ` ЯЗКИ <br />
           МІЖ КОМПАНІЯМИ ТА ЛЮДЬМИ
-        </div>
-        <div className={styles.aboutUsContainer__infoItem1}>
-          Ми не чекаємо випадкових вистрілів, які несуться на хайпі чи федах
-        </div>
-        <div className={styles.aboutUsContainer__infoItem2}>
-          У нашому розумінні, бренд-комунікація — це не красивий візуал, чи
-          відео у ТІк Ток, яке раптово завірусилось
-        </div>
-        <div className={styles.aboutUsContainer__infoItem3}>
-          Наш підхід — орієнтований <br /> на бізнес-цілі і завжди базується на
-          дослідженнях.
-          <br /> Перед створенням будь-якого кампейну — ми аналізуємо
-          середовище, потреби, контекст у якому існує компанія
-        </div>
-        <div className={styles.aboutUsContainer__infoItem4}>
-          І лише тоді починається креатив
         </div>
       </div>
     </section>
